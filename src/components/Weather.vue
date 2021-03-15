c3b1e2732e053fc4bb825ad7d6a33732d179339d
Florian Sinegre <sinegre.florian@gmail.com>
2021-03-15 16:03:27 +0100

    css page weather

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -30,22 +30,75 @@
             <v-col cols="6">
 
               <v-img
-                  src="https://cdn.vuetifyjs.com/images/cards/sun.png"
+                  v-if=" weather.weather[0].description == 'nuageux' "
+                  src="https://fyooyzbm.filerobot.com/v7/https://static01.nicematin.com/media/npo/large/2015/05/b8af1a5900556d2e5f4a643b11199ff7.png"
                   alt="Sunny image"
                   width="92"
               ></v-img>
-
+              <v-img
+                  v-if=" weather.weather[0].description == 'ciel dégagé' "
+                  src="https://www.icone-png.com/png/27/26672.png"
+                  alt="Sunny image"
+                  width="92"
+              ></v-img>
+              <v-img
+                  v-if=" weather.weather[0].description == 'pluie' "
+                  src="https://www.flaticon.com/svg/vstatic/svg/1146/1146858.svg?token=exp=1615820586~hmac=45bd922e2c5eb346cd08452ac519a66e"
+                  alt="Sunny image"
+                  width="92"
+              ></v-img>
+              <v-img
+                  v-if=" weather.weather[0].description == 'Légère pluie' "
+                  src="https://www.flaticon.com/svg/vstatic/svg/1146/1146858.svg?token=exp=1615820586~hmac=45bd922e2c5eb346cd08452ac519a66e"
+                  alt="Sunny image"
+                  width="92"
+              ></v-img>
             </v-col>
           </v-row>
         </v-card-text>
 
         <v-list-item>
           <v-list-item-icon>
-            <v-icon>mdi-send</v-icon>
+            <v-img src="https://www.flaticon.com/svg/vstatic/svg/615/615579.svg?token=exp=1615820726~hmac=45107a7ff55f97a28b38896a19003191"
+                   alt="Sunny image"
+                   width="10"></v-img>
+
           </v-list-item-icon>
           <v-list-item-subtitle>{{weather.wind.speed}} Km/H</v-list-item-subtitle>
         </v-list-item>
 
+        <v-list-item>
+          <v-list-item-icon>
+            <v-img src="https://www.flaticon.com/svg/vstatic/svg/4015/4015433.svg?token=exp=1615820418~hmac=0d1109f1456ca513e044df24d9f5ab8d"
+                   alt="Sunny image"
+                   width="10"></v-img>
+          </v-list-item-icon>
+          <v-list-item-subtitle>{{Math.round(weather.main.humidity)}} %</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-img src="https://www.flaticon.com/svg/vstatic/svg/556/556958.svg?token=exp=1615820654~hmac=8ad55754044dc8f3999ff7ba92a2b63d"
+                   alt="Sunny image"
+                   width="10"></v-img>
+          </v-list-item-icon>
+          <v-list-item-subtitle>{{Math.round(weather.main.pressure)}} hPa</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-img src="https://www.flaticon.com/svg/vstatic/svg/1684/1684375.svg?token=exp=1615821156~hmac=8a63e155c4941a0d9326572727141450"
+                   alt="Sunny image"
+                   width="10"></v-img>
+          </v-list-item-icon>
+          <v-list-item-subtitle>{{Math.round(weather.main.temp_max)}}°C </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-img src="https://www.flaticon.com/premium-icon/icons/svg/2322/2322701.svg"
+                   alt="Sunny image"
+                   width="10"></v-img>
+          </v-list-item-icon>
+          <v-list-item-subtitle>{{Math.round(weather.main.temp_min)}}°C </v-list-item-subtitle>
+        </v-list-item>
 
         <v-card-actions>
           <v-btn text>
